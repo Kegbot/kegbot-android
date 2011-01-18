3bb860cf56ad546fbe8a7622297d281860e5ab58
mike wakerly <opensource@hoho.com>
2011-01-17 17:52:18 -0800

    Initial Kegtap project.

---
+++ android/Kegtap/src/org/kegbot/kegtap/KegtapActivity.java
@@ -0,0 +1,12 @@
+package org.kegbot.kegtap;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class KegtapActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+    }
+}
---
+++ android/Kegtap/src/org/kegbot/kegtap/SplashScreenActivity.java
@@ -0,0 +1,7 @@
+package org.kegbot.kegtap;
+
+import android.app.Activity;
+
+public class SplashScreenActivity extends Activity {
+
+}
