bb1208cdaa7f4e6086815145e2b868a35c5fd842
mike wakerly <opensource@hoho.com>
2011-03-26 00:08:16 -0700

    Added INTERNET permission, renamed tap list variables, added API to KegtapActivity, loading tap data from API!

--- android/Kegtap/src/org/kegbot/kegtap/KegtapActivity.java
+++ android/Kegtap/src/org/kegbot/kegtap/KegtapActivity.java
@@ -1,5 +1,9 @@
 package org.kegbot.kegtap;
 
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.kegbot.api.KegbotApi;
+import org.kegbot.api.KegbotApiImpl;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,10 +12,16 @@ public class KegtapActivity extends Activity {
 
   public final String LOG_TAG = "KegtapActivity";
 
+  private KegbotApi mApi;
+  
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
     Log.w(LOG_TAG, "Main view!");
+    mApi = new KegbotApiImpl(new DefaultHttpClient(), "http://kegbot.net/sfo/api");
+    TapListFragment taps = (TapListFragment) getFragmentManager().findFragmentById(R.id.tap_list);
+    taps.setKegbotApi(mApi);
+    taps.loadTaps();
   }
 }
