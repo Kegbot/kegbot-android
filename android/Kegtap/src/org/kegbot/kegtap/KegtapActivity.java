e94c43b0996b931f4d4b0cd5a4ad4a8b7b60c4ca
mike wakerly <opensource@hoho.com>
2011-03-26 00:36:33 -0700

    Creating new instances of the api: not threadsafe.

--- android/Kegtap/src/org/kegbot/kegtap/KegtapActivity.java
+++ android/Kegtap/src/org/kegbot/kegtap/KegtapActivity.java
@@ -1,7 +1,6 @@
 package org.kegbot.kegtap;
 
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.kegbot.api.KegbotApi;
 import org.kegbot.api.KegbotApiImpl;
 
 import android.app.Activity;
@@ -11,21 +10,22 @@ import android.util.Log;
 public class KegtapActivity extends Activity {
 
   public final String LOG_TAG = "KegtapActivity";
-
-  private KegbotApi mApi;
   
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
     Log.w(LOG_TAG, "Main view!");
-    mApi = new KegbotApiImpl(new DefaultHttpClient(), "http://kegbot.net/sfo/api");
     TapListFragment taps = (TapListFragment) getFragmentManager().findFragmentById(R.id.tap_list);
-    taps.setKegbotApi(mApi);
+    taps.setKegbotApi(new KegbotApiImpl(new DefaultHttpClient(), getApiUrl()));
     taps.loadTaps();
     EventListFragment events = (EventListFragment) getFragmentManager().findFragmentById(
         R.id.event_list);
-    events.setKegbotApi(mApi);
+    events.setKegbotApi(new KegbotApiImpl(new DefaultHttpClient(), getApiUrl()));
     events.loadEvents();
+  }
+  
+  private String getApiUrl() {
+    return "http://kegbot.net/sfo/api";
   }
 }
