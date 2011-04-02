a400d747bc415dc4f3c41db6873efdc0b918185a
mike wakerly <opensource@hoho.com>
2011-04-02 08:50:52 -0700

    Action Bar.

--- android/Kegtap/src/org/kegbot/kegtap/KegtapActivity.java
+++ android/Kegtap/src/org/kegbot/kegtap/KegtapActivity.java
@@ -3,6 +3,7 @@ package org.kegbot.kegtap;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.kegbot.api.KegbotApiImpl;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,6 +17,8 @@ public class KegtapActivity extends Activity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
     Log.w(LOG_TAG, "Main view!");
+    setupActionBar();
+    
     TapListFragment taps = (TapListFragment) getFragmentManager().findFragmentById(R.id.tap_list);
     taps.setKegbotApi(new KegbotApiImpl(new DefaultHttpClient(), getApiUrl()));
     taps.loadTaps();
@@ -27,5 +30,12 @@ public class KegtapActivity extends Activity {
   
   private String getApiUrl() {
     return "http://kegbot.net/sfo/api";
+  }
+  
+  private void setupActionBar() {
+    ActionBar actionBar = getActionBar();
+    actionBar.setBackgroundDrawable(getResources().getDrawable(R.drawable.header_bg_square));
+    actionBar.setDisplayShowTitleEnabled(false);
+    actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME);
   }
 }
