3e8f993ba936d6d52522a4aa84ec8a4474d8737a
mike wakerly <opensource@hoho.com>
2012-05-31 22:25:52 -0700

    Threadsafety fixes for TapManager.

--- android/Kegtap/src/org/kegbot/core/Tap.java
+++ android/Kegtap/src/org/kegbot/core/Tap.java
@@ -100,4 +100,29 @@ public class Tap {
     return mRelayName;
   }
 
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((mMeterName == null) ? 0 : mMeterName.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Tap other = (Tap) obj;
+    if (mMeterName == null) {
+      if (other.mMeterName != null)
+        return false;
+    } else if (!mMeterName.equals(other.mMeterName))
+      return false;
+    return true;
+  }
+
 }
--- android/Kegtap/src/org/kegbot/core/TapManager.java
+++ android/Kegtap/src/org/kegbot/core/TapManager.java
@@ -18,7 +18,7 @@
  */
 package org.kegbot.core;
 
-import java.util.Collection;
+import java.util.Set;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableSet;
@@ -32,21 +32,21 @@ public class TapManager {
 
   private static TapManager sSingleton = null;
 
-  private final Collection<Tap> mTaps = Sets.newLinkedHashSet();
+  private final Set<Tap> mTaps = Sets.newLinkedHashSet();
 
   @VisibleForTesting
   protected TapManager() {
   }
 
-  public boolean addTap(final Tap newTap) {
+  public synchronized boolean addTap(final Tap newTap) {
     return mTaps.add(newTap);
   }
 
-  public boolean removeTap(final Tap tap) {
+  public synchronized boolean removeTap(final Tap tap) {
     return mTaps.remove(tap);
   }
 
-  public Tap getTapForMeterName(final String meterName) {
+  public synchronized Tap getTapForMeterName(final String meterName) {
     for (final Tap tap : mTaps) {
       if (meterName.equals(tap.getMeterName())) {
         return tap;
@@ -55,7 +55,7 @@ public class TapManager {
     return null;
   }
 
-  public Collection<Tap> getTaps() {
+  public synchronized Set<Tap> getTaps() {
     return ImmutableSet.copyOf(mTaps);
   }
 
